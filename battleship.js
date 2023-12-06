b481166653416986616273c15aa7a94a8f41efa3
Petar Patev <petarpatev@abv.bg>
2023-12-06 17:53:01 +0200

    Create loop, get and check user guess

--- battleship.js
+++ battleship.js
@@ -7,3 +7,16 @@ let hits = 0;
 let guesses = 0;
 let isSunk = false;
 
+while (isSunk == false) {
+
+    guess = prompt("Ready, aim, fire! (enter a number 0-6):");
+
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid cell number!");
+    }
+    else {
+        guesses = guesses + 1;
+    }
+
+    
+}
