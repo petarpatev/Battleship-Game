a5e1c0af3e71d3a4f183c71536d1eb69d4b519e6
Petar Patev <petarpatev@abv.bg>
2023-12-27 20:29:19 +0200

    Implementing the view object. Add displayMessage, displayHit, displayMiss methods.

--- battleship.js
+++ battleship.js
@@ -1,37 +1,62 @@
-let startLocation = Math.floor(Math.random() * 5);
-
-let location1 = startLocation;
-let location2 = location1 + 1;
-let location3 = location2 + 1;
-
-let guess;
-let hits = 0;
-let guesses = 0;
-let isSunk = false;
-
-while (isSunk == false) {
-
-    guess = prompt("Ready, aim, fire! (enter a number 0-6):");
-
-    if (guess < 0 || guess > 6) {
-        alert("Please enter a valid cell number!");
-    }
-    else {
-        guesses = guesses + 1;
-        if (guess == location1 || guess == location2 || guess == location3) {
-            alert("HIT");
-            hits = hits + 1;
-
-            if (hits == 3) {
-                isSunk = true;
-                alert("You sank my battleship!");
-            }
-        } else {
-            alert("MISS");
-        }
-    }
+var view = {
+    displayMessage: function (msg) {
+        var messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function (location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+    displayMiss: function (location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    },
 }
 
-let stats = "You took " + guesses + " guesses to sink the battleship, " +
-    "which means your shooting accuracy was " + (3 / guesses);
-alert(stats); 
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+view.displayMessage("Tap tap, is this thing on?");
+
+
+
+// let startLocation = Math.floor(Math.random() * 5);
+
+// let location1 = startLocation;
+// let location2 = location1 + 1;
+// let location3 = location2 + 1;
+
+// let guess;
+// let hits = 0;
+// let guesses = 0;
+// let isSunk = false;
+
+// while (isSunk == false) {
+
+//     guess = prompt("Ready, aim, fire! (enter a number 0-6):");
+
+//     if (guess < 0 || guess > 6) {
+//         alert("Please enter a valid cell number!");
+//     }
+//     else {
+//         guesses = guesses + 1;
+//         if (guess == location1 || guess == location2 || guess == location3) {
+//             alert("HIT");
+//             hits = hits + 1;
+
+//             if (hits == 3) {
+//                 isSunk = true;
+//                 alert("You sank my battleship!");
+//             }
+//         } else {
+//             alert("MISS");
+//         }
+//     }
+// }
+
+// let stats = "You took " + guesses + " guesses to sink the battleship, " +
+//     "which means your shooting accuracy was " + (3 / guesses);
+// alert(stats); 
