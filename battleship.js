6953358a21b1ea36934a196136d67a44af14b8ba
Petar Patev <petarpatev@abv.bg>
2023-12-31 17:46:53 +0200

    Implementing controller object and add 'parseGuess' function

--- battleship.js
+++ battleship.js
@@ -1,3 +1,23 @@
+function parseGuess(guess) {
+    var alphabetArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+    if (guess === null || guess.length !== 2) {
+        alert("Oops, please enter a letter and a number on the board.")
+    } else {
+        var firstChar = guess.charAt(0);
+        var row = alphabetArray.indexOf(firstChar);
+        var column = guess.charAt(1);
+        if (isNaN(row) || isNaN(column)) {
+            alert("Oops, that isn't on the board.")
+        } else if (row < 0 || row >= model.boardSize
+            || column < 0 || column >= model.boardSize) {
+            alert("Oops, that's off the board!")
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+}
+
 var view = {
     displayMessage: function (msg) {
         var messageArea = document.getElementById("messageArea");
@@ -62,15 +82,41 @@ var model = {
     }
 }
 
-model.fire("00");
-model.fire("01");
-model.fire("02");
-model.fire("42");
-model.fire("43");
-model.fire("44");
-model.fire("20");
-model.fire("30");
-model.fire("40");
+var controller = {
+    guesses: 0,
+    processGuess: function (guess) {
+        var location = parseGuess(guess);
+        if (location) {
+            this.guesses++;
+            var hit = model.fire(location);
+            if (hit && model.shipsSunk === model.numShips) {
+                view.displayMessage("You sank all my battleships, in " +
+                    this.guesses + " guesses.")
+            }
+        }
+    }
+}
+
+controller.processGuess("A0");
+controller.processGuess("A1");
+controller.processGuess("A2");
+controller.processGuess("C6");
+controller.processGuess("E2");
+controller.processGuess("E3");
+controller.processGuess("E4");
+controller.processGuess("C0");
+controller.processGuess("D0");
+controller.processGuess("E0");
+
+// model.fire("00");
+// model.fire("01");
+// model.fire("02");
+// model.fire("42");
+// model.fire("43");
+// model.fire("44");
+// model.fire("20");
+// model.fire("30");
+// model.fire("40");
 
 
 
